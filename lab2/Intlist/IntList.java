d9b61608271694734d03c26da7532d8ef99d7cfd
WJA <credential.helper>
2024-04-19 08:34:23 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if(A == null) {
+            return null;
+        }
         IntList ptr = A;
         while (ptr.rest != null) {
             ptr = ptr.rest;
