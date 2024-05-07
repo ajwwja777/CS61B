22002b5055f97bf610736ced31b35d487b8cee31
WJA <credential.helper>
2024-05-07 13:43:00 +0800

    Finished debug part

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,6 +1,46 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+/* 
+ * Your test should randomly call StudentArrayDeque and ArrayDequeSolution methods 
+ * until they disagree on an output.
+ * addFirst, addLast, removeFirst, and removeLast
+ */
 public class TestArrayDequeGold {
-    
+    @Test
+    public void addFirstTest() {
+
+    }
+
+    @Test
+    public void randomizedTest() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        int N = 50000;
+        String output = "";
+        for(int i = 0; i < N; i++) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                Integer randVal = StdRandom.uniform(0, 100);
+                ads.addFirst(randVal);
+                sad1.addFirst(randVal);
+                output += "\naddFirst(" + randVal + ")";
+                
+            } else if (operationNumber == 1) {
+                Integer randVal = StdRandom.uniform(0, 100);
+                ads.addLast(randVal);
+                sad1.addLast(randVal);
+                output += "\naddLast(" + randVal + ")";
+            } else if (operationNumber == 2 && ads.size() != 0) {
+                output += "\nremoveFirst()";
+                assertEquals(output, ads.removeFirst(), sad1.removeFirst());
+            } else if (operationNumber == 3 && ads.size() != 0) {
+                output += "\nremoveLast()";
+                assertEquals(output, ads.removeLast(), sad1.removeLast());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    }
 }
---
+++ proj1gold/jhwGold.java
@@ -0,0 +1,61 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by JunhaoW on 04/08/2019
+ */
+
+public class jhwGold {
+
+  @Test
+  public void testSad() {
+    StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+    ArrayDequeSolution<Integer> ans = new ArrayDequeSolution<>();
+
+    String output = "";
+
+    int count = 0;
+    while (true) {
+      double randNum = StdRandom.uniform();
+
+      if (randNum < 0.25) {
+        sad.addFirst(count);
+        ans.addFirst(count);
+        // Integer actual = sad.get(0);
+        // Integer expected = ans.get(0);
+        output += "\naddFirst(" + count + ")";
+        // assertEquals(output, expected, actual);
+
+      } else if (randNum < 0.5) {
+        sad.addLast(count);
+        ans.addLast(count);
+        // Integer actual = sad.get(sad.size() - 1);
+        // Integer expected = ans.get(sad.size() - 1);
+        output += "\naddLast(" + count + ")";
+        // assertEquals(output, expected, actual);
+
+      } else if (randNum < 0.75) {
+        Integer actual = sad.removeFirst();
+        Integer expected = ans.removeFirst();
+        if (actual == null || expected == null) {
+          continue;
+        }
+        output += "\nremoveFirst()";
+        assertEquals(output, expected, actual);
+
+      } else {
+        Integer actual = sad.removeLast();
+        Integer expected = ans.removeLast();
+        if (actual == null || expected == null) {
+          continue;
+        }
+        output += "\nremoveLast()";
+        assertEquals(output, expected, actual);
+      }
+
+      count += 1;
+    }
+
+  }
+}
